b879137198470152eb2febccac285dc6374e5646
Brian Rinaldi <brian.rinaldi@telerik.com>
2016-01-15 18:55:11 +0000

    Changes made by Brian Rinaldi (brian.rinaldi@telerik.com).

--- pluginSampleApp/js/app.js
+++ pluginSampleApp/js/app.js
@@ -7,7 +7,7 @@ angular.module('pluginSampleApp', ['ionic', 'ui.router'])
       templateUrl: 'templates/home.html',
       controller: 'PluginCtrl'
     })
-    .state('app.viewimage', {
+    .state('viewimage', {
       url: '/viewimage',
       templateUrl: 'templates/viewImage.html',
       controller: 'PluginCtrl'
@@ -18,7 +18,33 @@ angular.module('pluginSampleApp', ['ionic', 'ui.router'])
 .controller('PluginCtrl', function($scope, $state) {
     // open the barcode scanner
     $scope.openBarcode = function() {
-        $state.go('app.viewimage');
+        cordova.plugins.barcodeScanner.scan(
+
+            // success callback function
+            function (result) {
+                //https://45.media.tumblr.com/ff06c2c5c03293265f24d8721571eee4/tumblr_n6r8b62SJC1tbhzz6o1_400.gif
+                $scope.imageURL = 'https://45.media.tumblr.com/ff06c2c5c03293265f24d8721571eee4/tumblr_n6r8b62SJC1tbhzz6o1_400.gif';
+                $state.go('viewimage');
+                // wrapping in a timeout so the dialog doesn't free the app
+                //setTimeout(function() {
+                //    alert("We got a barcode\n" +
+                //          "Result: " + result.text + "\n" +
+                //          "Format: " + result.format + "\n" +
+                //          "Cancelled: " + result.cancelled);                            
+                //}, 0);
+            },
+
+            // error callback function
+            function (error) {
+                alert("Scanning failed: " + error);
+            },
+
+            // options object
+            {
+                "preferFrontCamera" : false,
+                "showFlipCameraButton" : true
+            }
+         );
         
     };
 })
