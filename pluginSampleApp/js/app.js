f07aa5700c1b4f0853462cc898007f52e27743a5
Brian Rinaldi <brian.rinaldi@telerik.com>
2016-01-21 19:46:04 +0000

    Changes made by Brian Rinaldi (brian.rinaldi@telerik.com).

--- pluginSampleApp/js/app.js
+++ pluginSampleApp/js/app.js
@@ -8,56 +8,12 @@ angular.module('pluginSampleApp', ['ionic', 'ui.router'])
       url: '/app',
       templateUrl: 'templates/home.html',
       controller: 'PluginCtrl'
-    })
-    .state('viewimage', {
-      url: '/viewimage',
-      templateUrl: 'templates/viewImage.html',
-      controller: 'ImageCtrl'
     });
     $urlRouterProvider.otherwise('/app');
 })
 
 .controller('PluginCtrl', function($scope, $state) {
-    // open the barcode scanner
-    $scope.openBarcode = function() {
-        cordova.plugins.barcodeScanner.scan(
-
-            // success callback function
-            function (result) {
-                scannedURL = result.text;
-                $state.go('viewimage');
-            },
-
-            // error callback function
-            function (error) {
-                alert("Scanning failed: " + error);
-            },
-
-            // options object
-            {
-                "preferFrontCamera" : false,
-                "showFlipCameraButton" : true
-            }
-         );
-        
-    };
-})
-
-.controller('ImageCtrl', function($scope) {
-    $scope.imageURL = scannedURL;
-    $scope.share = function() {
-        plugins.socialsharing.share(null, null, scannedURL, null, function() {
-            var options = {
-              id:         1,
-              at:         new Date(),
-              text:       "Your friends will thank you",
-              title:      "You shared the image",
-              autoClear:  true
-            };
-
-            cordova.plugins.notification.local.schedule(options);
-        });
-    }
+	
 })
 
 .run(function($ionicPlatform) {
